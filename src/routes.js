cc178925ee49cb662b8efa65ba71c0f84a5e9d92
Rusiru Abhisheak <rusiru.bandara@redot.global>
2021-07-29 20:51:38 +0530

    Add doctor CRUD API endpoints :rocket:

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,13 @@
-import { addDoctor, searchDoctors } from './controller/doctor.controller';
+import {
+  addDoctor,
+  searchDoctors,
+  updateDoctor,
+  deleteDoctor,
+} from './controller/doctor.controller';
 
 export default function (app) {
   app.post('/doctor/add', addDoctor);
   app.get('/doctor/search', searchDoctors);
+  app.put('/doctor/update/:id', updateDoctor);
+  app.delete('/doctor/delete/:id', deleteDoctor);
 }
