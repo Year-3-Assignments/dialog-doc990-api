637186480025d3260c1b342f2f6c7d5d4a78a56a
rusiruavb <rusiruavb98@gmail.com>
2021-07-29 15:27:03 +0530

    Implement user database model

---
+++ src/enums.js
@@ -0,0 +1,19 @@
+export default {
+  user: {
+    NOT_FOUND: 'User account not found',
+    ALREADY_EXIST: 'User account already exist',
+    LOGIN_SUCCESS: 'User login successful',
+    LOGIN_FAIL: 'User login failed',
+    PASSWORD_NOT_MATCH: 'Password is not matched',
+    CREATENTIAL_REQUIRED: 'User credential required',
+    CREATE_SUCCESS: 'Account created successfully',
+    CREATE_ERROR: 'Error with create user account',
+    UPDATE_SUCCESS: 'User account update successfully',
+    UPDATE_ERROR: 'Error with update user account',
+    DELETE_SUCCESS: 'User account delete successfully',
+    DELETE_ERROR: 'Error with delete user account',
+  },
+  doctor: {
+    NOT_FOUND: 'Doctor details not found',
+  },
+};
