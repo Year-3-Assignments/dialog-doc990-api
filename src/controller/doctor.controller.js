e9056ed49373ae5aa675da8dd79368695d37aca1
RaveenaWickramasinghe <raveenawickramasinghe@gmail.com>
2021-08-15 14:09:38 +0530

    Doctor Controller class done.

--- src/controller/doctor.controller.js
+++ src/controller/doctor.controller.js
@@ -1,19 +1,26 @@
 import Doctor from '../model/Doctor';
 import enums from '../enums';
+import responseHandler from '../response/response.handler';
 
 export async function addDoctor(req, res) {
-  if (req.body) {
-    const doctor = new Doctor(req.body);
-    doctor
-      .save()
-      .then((data) => {
-        LOG.info(enums.doctor.CREATE_SUCCESS);
-      })
-      .catch((error) => {
-        LOG.info(enums.doctor.CREATE_ERROR);
-        responseHandler.handleError(res, error.message);
-      });
-  }
+  let doctorData = new Doctor(req.body);
+  const doctor = new Doctor(doctorData);
+  await doctor
+    .save()
+    .then((data) => {
+      responseHandler.respond(res, data);
+    })
+    .catch((error) => {
+      responseHandler.handleError(res, error.message);
+    });
 }
 
-export async function getAllDoctors() {}
+export async function getAllDoctors() {
+  await Doctor.find({})
+    .then((doctors) => {
+      responseHandler.respond(res, data);
+    })
+    .catch((error) => {
+      response.handleError(res, error.message);
+    });
+}
